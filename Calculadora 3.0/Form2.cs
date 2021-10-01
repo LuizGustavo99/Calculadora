7b425e84dfba6a9ff61542e78aa17488b3e1b1ea
Luiz Gustavo <luiz.gsrod99@gmail.com>
2021-09-30 22:56:21 -0300

    Calculadora teste2

--- Calculadora 3.0/Form2.cs
+++ Calculadora 3.0/Form2.cs
@@ -35,7 +35,7 @@ namespace Calculadora_3._0
 
         private void button2_Click(object sender, EventArgs e)
         {
-            float num1, num2, resultado, teste;
+            float num1, num2, resultado;
 
             num1 = float.Parse(textBox1.Text);
             num2 = float.Parse(textBox2.Text);
